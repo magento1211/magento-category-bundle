a903d32e829ecdc5480a8d5e98749bec2061187e
magento1211 <hoffmanotto16@gmail.com>
2021-10-29 16:22:28 +0200

    Create PropertyForm class for loading the form fields and their values

---
+++ src/Resources/public/js/property/property-form.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+
+type CategoryInfo = {
+    categoryCode: string
+};
+
+class PropertyForm extends React.Component<CategoryInfo> {
+    state = {
+        propertyValues: {}
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <input id={'flagbit_category_properties_json'} hidden={true} />
+            </React.Fragment>
+        );
+    }
+}
+
+export default PropertyForm;
--- src/Resources/public/js/property/property-forms-renderer.tsx
+++ src/Resources/public/js/property/property-forms-renderer.tsx
@@ -1,7 +1,9 @@
+import PropertyForm from './property-form';
+
 define(['react', 'react-dom'], function (
     React, ReactDOM
 ) {
-    return (domContainer) => {
-        ReactDOM.render(<input value={'test'} name={'test'} id={'test'} hidden={false} />, domContainer);
+    return (domContainer, categoryCode: string) => {
+        ReactDOM.render(<PropertyForm categoryCode={categoryCode} />, domContainer);
     };
 });
