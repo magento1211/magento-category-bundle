3718592bdfe862fcb2dc88ea26ead1d83c7b8c16
magento1211 <hoffmanotto16@gmail.com>
2022-01-26 16:55:34 +0100

    Complete coverage for property type directory

---
+++ jest/public/js/property/type/property-dto.test.ts
@@ -0,0 +1,48 @@
+import PropertyDto from '../../../../../src/Resources/public/js/property/type/property-dto';
+
+const createConfig = () => ({
+    isLocalizable: false,
+    labels: { null: 'label' },
+    config: {},
+    type: 'text'
+});
+
+describe('Property data transfer object', function () {
+    test('Value properties', function () {
+        const config = createConfig();
+
+        const dto = new PropertyDto('value', 'code', null, config, jest.fn());
+
+        expect(dto.value).toBe('value');
+        expect(dto.code).toBe('code');
+        expect(dto.locale).toBe(null);
+        expect(dto.config).toBe(config);
+
+        const dto2 = new PropertyDto('value', 'code', 'de_DE', config, jest.fn());
+
+        expect(dto2.locale).toBe('de_DE');
+    });
+
+    test('Create ID', function () {
+        const dto = new PropertyDto('value', 'code', 'de_DE', createConfig(), jest.fn());
+
+        expect(dto.createId()).toBe('flagbit_id_code_de_DE');
+
+        const dto2 = new PropertyDto('value', 'code', null, createConfig(), jest.fn());
+
+        expect(dto2.createId()).toBe('flagbit_id_code_');
+    });
+
+    test('Update value', function () {
+        const onChange = jest.fn();
+
+        const dto = new PropertyDto('value', 'code', 'de_DE', createConfig(), onChange);
+
+        dto.updateValue('foo');
+
+        expect(onChange.mock.calls.length).toBe(1);
+        expect(onChange.mock.calls[0][0]).toBe('code');
+        expect(onChange.mock.calls[0][1]).toBe('de_DE');
+        expect(onChange.mock.calls[0][2]).toBe('foo');
+    });
+});
---
+++ jest/public/js/property/type/text.test.tsx
@@ -0,0 +1,56 @@
+import * as React from 'react';
+import { shallow } from 'enzyme';
+import text from '../../../../../src/Resources/public/js/property/type/text';
+import PropertyDto from '../../../../../src/Resources/public/js/property/type/property-dto';
+
+describe('Text property type', function () {
+    test('Rendering the value', function () {
+        const onChange = jest.fn();
+
+        const config = {
+            isLocalizable: false,
+            labels: { null: 'label' },
+            config: {},
+            type: 'text',
+        };
+
+        const textInstance = text();
+        const dto = new PropertyDto('value', 'code', null, config, onChange);
+
+        const Text = () => textInstance.render(dto);
+
+        // @ts-ignore
+        const renderedView = shallow(<Text />);
+
+        const contentInputField = renderedView.find('input').first();
+        expect(contentInputField.props().value).toBe('value');
+        expect(contentInputField.props().id).toBe('flagbit_id_code_');
+    });
+
+    test('Changing the value', function () {
+        const onChange = jest.fn();
+
+        const config = {
+            isLocalizable: false,
+            labels: { null: 'label' },
+            config: {},
+            type: 'text',
+        };
+
+        const textInstance = text();
+        const dto = new PropertyDto('value', 'code', null, config, onChange);
+
+        const Text = () => textInstance.render(dto);
+
+        // @ts-ignore
+        const renderedView = shallow(<Text />);
+
+        const contentInputField = renderedView.find('input').first();
+        contentInputField.simulate('change', { target: { value: 'value2' }});
+
+        expect(onChange.mock.calls.length).toBe(1);
+        expect(onChange.mock.calls[0][0]).toBe('code');
+        expect(onChange.mock.calls[0][1]).toBe(null);
+        expect(onChange.mock.calls[0][2]).toBe('value2');
+    });
+});
