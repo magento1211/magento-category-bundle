015ab8b1dc471616afabf6dfd19dfc25393fe40a
magento1211 <hoffmanotto16@gmail.com>
2022-01-28 12:11:57 +0100

    Create coverage for post-config

---
+++ jest/public/js/property/api/post-config.test.ts
@@ -0,0 +1,19 @@
+import postConfig from '../../../../../src/Resources/public/js/property/api/post-config';
+
+jest.mock(
+    'pim/router',
+    () => ({
+        generate: (route: string, params: any) => {
+            expect(route).toBe('flagbit_category.internal_api.category_config_post');
+            expect(params).toEqual({ identifier: 1 });
+        },
+    }),
+    { virtual: true }
+);
+
+// TODO: This needs a better test case because post-config.ts is just glue code.
+describe('Config POST method', function () {
+    test('post config', function () {
+        postConfig.post({});
+    });
+});
